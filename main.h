36b413295dd76a0ffdb8158bcefd3bfbd7ecc857
trozymotto <ryan.trosvig@gmail.com>
2015-04-13 21:12:37 -0500

    Initial

---
+++ main.h
@@ -0,0 +1,11 @@
+/*******************************************
+*
+* Header file for Main program.
+*
+*******************************************/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+void print_adc_vals(void);
+
+#endif //__MAIN_H
