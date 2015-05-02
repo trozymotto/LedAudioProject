737d603f97e97ce943f16a40e6fe559a2adef0bb
bande2005 <andersen.barry@gmail.com>
2015-05-02 17:54:31 -0500

    updates

--- main.c
+++ main.c
@@ -34,6 +34,7 @@ typedef enum {
 // Function prototypes
 void init_adc(void);
 void print_adc_vals();
+int8_t abs_int8_t(int8_t data);
 
 // GLOBALS
 char send_buffer[32];
@@ -48,6 +49,7 @@ volatile long global_adc_counter = 0;
 //unsigned int dataArray1[ARRAY_LEN];
 tCaptureBank  mBank = BANK1;
 unsigned char samples = 0;
+int16_t sample;
 
 // spectrum downsample data
 #define FFT_BINS 8
@@ -81,6 +83,8 @@ int8_t       spectrum[FFT_N/2]; // Spectrum output buffer
 int main(void)
 {
   bool led_matrix_on = 0;
+  int sample_sum = 0;
+  int sample_avg = 0;
 
     // Initialization here.
     lcd_init_printf();	// required if we want to use printf() for LCD printing
@@ -91,7 +95,7 @@ int main(void)
 
     //enable interrupts
     sei();
-    
+
     //init the LED Matrix
     Adafruit_GFX(32, 32);
   	init(16,LED_A, LED_B, LED_C, LED_D, LED_CLK, LED_LAT, LED_OE, false,32);
@@ -100,30 +104,38 @@ int main(void)
     //start_analog_conversion(CHANNEL_A);  // start initial conversion
     while (1)
     {
-      
-        
+
+
         if (printAdc)
         {
             printAdc = 0;
-            
-            if(mBank == BANK2){
-                fix_fft((char*)bfly_buff, (char*)capture1, 7, 0);
-            }
-            else{
-                fix_fft((char*)bfly_buff, (char*)capture2, 7, 0);
-            }
-            print_adc_vals();
-         
-            // Downsample spectrum output to 8 columns:
             volatile int x = 0;
             uint16_t sum;
             int i;
             int samplesPerBin;
+            if(mBank == BANK2){
+                fix_fft((char*)bfly_buff, (char*)capture1, 7, 0);
+                sample_sum = 0;
+                for(i=0;i<128;i++)
+                    sample_sum += abs_int8_t(capture1[i]);
+                sample_avg = sample_sum/8;
+            }
+            else{
+                fix_fft((char*)bfly_buff, (char*)capture2, 7, 0);
+                sample_sum = 0;
+                for(i=0;i<128;i++)
+                    sample_sum += abs_int8_t(capture2[i]);
+                sample_avg = sample_sum/8;
+            }
+            //print_adc_vals();
+
+            // Downsample spectrum output to 8 columns:
+
             for(x=0; x<FFT_BINS; x++) {
                 sum = 0;
                 samplesPerBin = FFT_N/FFT_BINS;
                 for(i=0; i<samplesPerBin; i++)
-                    sum += abs(bfly_buff[(x*samplesPerBin)+i]);
+                    sum += abs_int8_t(bfly_buff[(x*samplesPerBin)+i]);
                 nowSpectrum[x] = sum;
                 // Shift the data in the history buffer
                 for (i=0; i<PAST_NUM-1; i++)
@@ -135,20 +147,48 @@ int main(void)
                 for(i=0;i<PAST_NUM;i++){
                     avgSpectrum[x] += pastSpectrum[x][i];
                 }
-                avgSpectrum[x] = avgSpectrum[x]/PAST_NUM;   
+                avgSpectrum[x] = avgSpectrum[x]/PAST_NUM;
             }
             //fft_execute(bfly_buff);          // Process complex data
             //fft_output(bfly_buff, spectrum); // Complex -> spectrum
-        }
-    
-        print_adc_vals();
+
+        //print_adc_vals();
+        
+        fillRect(0, 0, 4, 32, Color333(0, 0, 0));
+        fillRect(0, 0, 4, nowSpectrum[0], Color333(7, 0, 0));
+
+        fillRect(4, 0, 4, 32, Color333(0, 0, 0));
+        fillRect(4, 0, 4, nowSpectrum[1], Color333(7,4,0));
+
+        fillRect(8, 0, 4, 32, Color333(0, 0, 0));
+        fillRect(8, 0, 4, nowSpectrum[2], Color333(0,7,0));
+
+        fillRect(12, 0, 4, 32, Color333(0, 0, 0));
+        fillRect(12, 0, 4, nowSpectrum[3], Color333(0,7,7));
+
+        fillRect(16, 0, 4, 32, Color333(0, 0, 0));
+        fillRect(16, 0, 4, nowSpectrum[4], Color333(0,4,7));
+
+        fillRect(20, 0, 4, 32, Color333(0, 0, 0));
+        fillRect(20, 0, 4, nowSpectrum[5], Color333(0,0,7));
+
+        fillRect(24, 0, 4, 32, Color333(0, 0, 0));
+        fillRect(24, 0, 4, nowSpectrum[6], Color333(4,0,7));
+
+        fillRect(28, 0, 4, 32, Color333(0, 0, 0));
+        fillRect(28, 0, 4, nowSpectrum[7], Color333(7,0,4));
+
+
+        //fillRect(0, 0, 32, 32, Color333(0, 0, 0));
+        //fillRect(0, 0, 32, sample_avg, Color333(7, 0, 0));
+
         // Pass avgSpectrum into the LED bar update
-        
-        
+
+}
         //lcd_goto_xy(0,0);
         //printf("ADC:%ld", global_adc_counter);
 
-        
+
         if (display_shapes)
           test_shapes();
 
@@ -178,7 +218,7 @@ void print_adc_vals(void)
 	int length;
 	char tempBuffer[64];
     wait_for_sending_to_finish();
-    
+
     // Print the header
     //if(mBank == BANK2){
     //    length = sprintf( tempBuffer, "Capture1:\r\n");
@@ -190,19 +230,19 @@ void print_adc_vals(void)
     //wait_for_sending_to_finish();
 
     // View the spectrum data
-    length = snprintf( tempBuffer, 64, "%d,%d,%d,%d,%d,%d,%d,%d,"
-                                       "%d,%d,%d,%d,%d,%d,%d,%d\r\n",
-                  nowSpectrum[0], avgSpectrum[0],
-                  nowSpectrum[1], avgSpectrum[1],
-                  nowSpectrum[2], avgSpectrum[2],
-                  nowSpectrum[3], avgSpectrum[3],
-                  nowSpectrum[4], avgSpectrum[4],
-                  nowSpectrum[5], avgSpectrum[5],
-                  nowSpectrum[6], avgSpectrum[6],
-                  nowSpectrum[7], avgSpectrum[7]);
+    length = snprintf( tempBuffer, 64, //"%d,%d,%d,%d,%d,%d,%d,%d,"
+                                       "%d,%d,%d,%d,%d,%d,%d,%d,%d\r\n",
+                  nowSpectrum[0], //avgSpectrum[0],
+                  nowSpectrum[1], //avgSpectrum[1],
+                  nowSpectrum[2], //avgSpectrum[2],
+                  nowSpectrum[3], //avgSpectrum[3],
+                  nowSpectrum[4], //avgSpectrum[4],
+                  nowSpectrum[5], //avgSpectrum[5],
+                  nowSpectrum[6], //avgSpectrum[6],
+                  nowSpectrum[7],sample);//, //avgSpectrum[7]);
     print_usb(tempBuffer, length);
     wait_for_sending_to_finish();
-/*    
+/*
     // View the current values in the array
     for(i = 0; i < FFT_N; i+=5)
     {
@@ -213,10 +253,10 @@ void print_adc_vals(void)
                                                "%d,%d\r\n"
                                                "%d,%d\r\n",
                       capture1[i],    bfly_buff[i],
-                      capture1[i+1],  bfly_buff[i+1], 
-                      capture1[i+2],  bfly_buff[i+2], 
-                      capture1[i+3],  bfly_buff[i+3], 
-                      capture1[i+4],  bfly_buff[i+4]); 
+                      capture1[i+1],  bfly_buff[i+1],
+                      capture1[i+2],  bfly_buff[i+2],
+                      capture1[i+3],  bfly_buff[i+3],
+                      capture1[i+4],  bfly_buff[i+4]);
         }
         else{
             length = snprintf( tempBuffer, 64, "%d,%d\r\n"
@@ -225,12 +265,12 @@ void print_adc_vals(void)
                                                "%d,%d\r\n"
                                                "%d,%d\r\n",
                       capture2[i],    bfly_buff[i],
-                      capture2[i+1],  bfly_buff[i+1], 
-                      capture2[i+2],  bfly_buff[i+2], 
-                      capture2[i+3],  bfly_buff[i+3], 
+                      capture2[i+1],  bfly_buff[i+1],
+                      capture2[i+2],  bfly_buff[i+2],
+                      capture2[i+3],  bfly_buff[i+3],
                       capture2[i+4],  bfly_buff[i+4]);
         }
-        
+
         print_usb(tempBuffer, length);
         wait_for_sending_to_finish();
     }
@@ -241,8 +281,8 @@ void print_adc_vals(void)
 void init_adc(void)
 {
     set_analog_mode(MODE_10_BIT);    // 8-bit analog-to-digital conversions
-    
-    
+
+
 /*    ADMUX = (1 << REFS0);
     //ADSC
     //23.4 Page 237
@@ -250,7 +290,7 @@ void init_adc(void)
     //23.9.2 Page 250
     //7 ADEN, 6 ADSC, 5 ADATE, 4 ADIF, 3 ADIE, 2 ADPS2, 1 ADPS1 0 ADPS0
     ADCSRA = (1 << ADEN) | (1 << ADSC) | (1 << ADATE) | (0 << ADIF) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-    
+
     ADCSRB = 0;                // Free run mode, no high MUX bit
     DIDR0 = (1 << ADC0D);
     TIMSK0 = 0;                // Timer0 off*/
@@ -260,9 +300,9 @@ void init_adc(void)
 ISR(TIMER0_COMPA_vect)
 {
     static const int16_t noiseThreshold = 4;
-    int16_t              sample         = analog_read(0);
+    sample = analog_read(0);
 
-    if((sample > (512-noiseThreshold)) && (sample < (512+noiseThreshold))) 
+    if((sample > (512-noiseThreshold)) && (sample < (512+noiseThreshold)))
         sample = 0;
     else
         sample -= 512; // Sign-convert for FFT; -512 to +511
@@ -281,11 +321,11 @@ ISR(TIMER0_COMPA_vect)
     default:
         break;
     }
-    
+
     // if all samples have been taken...
     // Set the flag to run the FFT
     // Flip to the other bank
-    if (samples >= FFT_N)           
+    if (samples >= FFT_N)
     {
         printAdc = 1;
         samples = 0;
@@ -301,8 +341,14 @@ ISR(TIMER0_COMPA_vect)
 }
 
 ISR(ADC_vect) {
-    
+
     global_adc_counter++;
-    
+
 }
 
+int8_t abs_int8_t(int8_t data)
+{
+    if(data < 0)
+        return 0-data;
+    return data;
+}
